a8a8e694f407c7f52b72ec40869da3b5ff975364
mailtodighade <mailtodighade@gmail.com>
2022-04-30 11:23:24 +0530

    season display app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//import component from onther file.
+import App from "./components/App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+///create a react component.
+
+//render react component on the browser.
+const root = ReactDOM.createRoot(document.getElementById("neeraj"));
+root.render(<App />);
